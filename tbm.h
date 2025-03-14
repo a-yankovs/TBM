1e0a2c46c9075aa4c2b2b86ed5f8ae1b1e23b204
Alexandra <150750157+a-yankovs@users.noreply.github.com>
2025-03-14 16:04:09 -0700

    Update tbm.h

--- tbm.h
+++ tbm.h
@@ -1,42 +1,69 @@
-// file that defines all of our pins, pins have yet to be chosen 
-#define RX 
-#define TX 
-#define ESTOPCTRL_PIN 
-#define MOTORCTRL_PIN 
-#define PUMPCTRL_PIN 
-#define BENTCTRL_PIN 
+#include <Arduino.h>
+#include "tbm.h"
 
-#define ESTOPSENSE_PIN 
-#define MOTOR_TEMP_PIN 
-#define PUMP_TEMP_PIN 
-#define FLOW_IN_PIN 
-#define FLOW_OUT_PIN 
-#define MOTORSENSE_PIN 
-#define PUMPSENSE_PIN 
+// ---------------------------------------------------------
+//  Setup for state machine
+// ---------------------------------------------------------
+void state_setup() {
+  Serial.println("state_setup: Entering CONFIG");
+  systemData.state = STATE_CONFIG;
+}
 
+// ---------------------------------------------------------
+//  Check if all outputs are physically off
+// ---------------------------------------------------------
+bool checkStopped() {
+  if (digitalRead(MOTORCTRL_PIN) != LOW)  return false;
+  if (digitalRead(PUMPCTRL_PIN)  != LOW)  return false;
+  if (digitalRead(BENTCTRL_PIN)  != LOW)  return false;
+  return true;
+}
+// ---------------------------------------------------------
+//  Shuts off TBM motor, estop, bentonite pump, and water pump 
+// ---------------------------------------------------------
+void stoppingTBM() { 
+  digitalWrite(ESTOPCTRL_PIN, HIGH);
+  digitalWrite(MOTORCTRL_PIN, LOW);
+  digitalWrite(PUMPCTRL_PIN, LOW);
+  digitalWrite(BENTCTRL_PIN, LOW);
+}
+// ---------------------------------------------------------
+//  Finite state machine for TBM 
+//  (Transitions between CONFIG, RUNNING, ERROR, STOP)
+// ---------------------------------------------------------
+void state_loop() {
+  switch(systemData.state) {
+    case STATE_CONFIG:
+      Serial.println("STATE_CONFIG: Checking if system is stopped...");
+      systemData.state = STATE_RUNNING;
+      break;
 
-#define SENSOR_API "/sensor"
-#define CONFIG_API "/config"
+    case STATE_RUNNING:
+      if (systemData.motor_temp.value >= maxTemp || systemData.estop_button.value == 1) {
+        stoppingTBM(); 
+        systemData.state = STATE_STOP;
+        break;  
+      } 
+      digitalWrite(ESTOPCTRL_PIN, LOW);
+      digitalWrite(MOTORCTRL_PIN, HIGH);
+      digitalWrite(PUMPCTRL_PIN, HIGH);
+      digitalWrite(BENTCTRL_PIN, HIGH);
+      Serial.printf("RUNNING: Motor temp = %d C\n", systemData.motor_temp.value);
+      break;
 
-
-#define STOP "stop"
-#define START "start"
-#define RUNNING "running"
-#define ERROR "error"
-
-typedef enum { 
-    NO_MESSAGE = 0, 
-    TBM_INIT = 1,  
-    TBM_START = 2,  
-    TBM_STOP = 3,  
-    TBM_ERROR = 4,  
-    TBM_DATA = 5,  
-} MessageID; 
-
-MessageID incomingMessage = NO_MESSAGE; 
-    
-struct Sensor {
-    bool active;
-    int value;   // can be an array if we need multiple values.
-    signed int timestamp;
-};
+    case STATE_STOP:
+      Serial.println("STATE_STOP: Checking if physically off...");
+      int i = 0;
+      while (!checkStopped() || i != 100) {
+        stoppingTBM(); 
+        i++;  
+      } 
+      if (!checkStopped()) { 
+        Serial.println("TRIED 100 TIMES TO STOP TBM - PULL THE PLUG!"); 
+      }
+      if (systemData.estop_button.value == 0 && systemData.motor_temp.value < maxTemp) {
+        systemData.state = STATE_CONFIG;
+      }
+      break;
+  }
+}
